a061015563ea6f44fa01bcbd6b0b6857bbbf4e4c
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-02-14 18:28:39 -0500

    Création Prefab Liane
    Modification du prefab de liane rigide temporaire et déplacement dans le dossier prefab.

--- RootOfLife/Assets/Scripts/Player/LadderClimb.cs
+++ RootOfLife/Assets/Scripts/Player/LadderClimb.cs
@@ -4,69 +4,70 @@ using UnityEngine;
 
 public class LadderClimb : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed = 5;
     private float playerXInput;
     private float jumpForce;
+
+    public bool movingUp;
+    public bool movingDown;
+
+    public Animator animator;
+    
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        movingUp = false;
+        movingDown = false;
+        
       
     }
     
     private void Update()
     {
-       
+       if (Input.GetAxis("Vertical") > 0)
+        {
+            movingUp = true;
+            movingDown = false;
+        }
+
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            movingUp = false;
+            movingDown = true;
+        }
+
+        else
+        {
+            movingUp = false;
+            movingDown = false;
+        }
+
     }
 
-    /*private void OnTriggerStay(Collider other)
-    {
-        //other.GetComponent<Rigidbody>().useGravity = false;
-
-        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
-        {
-            other.GetComponent<Rigidbody>().velocity = new Vector3(0, speed, 0);
-        }
-
-        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
-        {
-            other.GetComponent<Rigidbody>().velocity = new Vector3(0, -speed, 0);
-        }
-    }*/
-
-    /*private void OnTriggerStay(Collider other)
-    {
-        other.GetComponent<Rigidbody>().isKinematic = true;
-
-        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
-        {
-            other.GetComponent<Transform>().position = new Vector3(other.GetComponent<Transform>().position.x, speed, other.GetComponent<Transform>().position.z);
-        }
-
-        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
-        {
-            other.GetComponent<Transform>().position = new Vector3(other.GetComponent<Transform>().position.x, -speed, other.GetComponent<Transform>().position.z);
-        }
-    }*/
 
     private void OnTriggerStay(Collider other)
     {
         playerXInput = other.GetComponent<PlayerController>().xInput * other.GetComponent<PlayerController>().speed;
         jumpForce = other.GetComponent<PlayerController>().playerJumpForce;
 
-        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
+        if (other.tag == "Player" && movingUp == true)
         {
             
             other.GetComponent<Rigidbody>().velocity = new Vector3(0, speed, 0);
             other.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+
+            animator.SetBool("isClimbing", true);
         }
 
-        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        else if (other.tag == "Player" && movingDown == true)
         {
             other.GetComponent<Rigidbody>().velocity = new Vector3(0, -speed, 0);
             other.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+
+            animator.SetBool("isClimbing", true);
         }
 
         else
@@ -75,12 +76,17 @@ public class LadderClimb : MonoBehaviour
             if (other.GetComponent<PlayerController>().isGrounded == false )
             {
                 other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+
+                animator.SetBool("isClimbing", true);
+
             }
             
             if (Input.GetButton("Jump"))
             {
                 other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
                 other.GetComponent<Rigidbody>().AddForce(playerXInput, jumpForce, 0);
+
+                animator.SetBool("isClimbing", false);
             }
 
         }
@@ -89,6 +95,9 @@ public class LadderClimb : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+
+        animator.SetBool("isClimbing", false);
+
     }
 
 }
