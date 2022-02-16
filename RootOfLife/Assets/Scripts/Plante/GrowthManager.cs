7a47ab62d2696bd4415e08e97d6074681d5aebc6
Hugo <kerdranvat.hugo@gmail.com>
2022-02-16 22:12:37 +0100

    Ajustement de la destruction des capsule et du maxCap
    - TestScene modifié
    - Script GrowthManager fortement modifié
    -  Ajustement Coroutine et destruction des prefab

--- RootOfLife/Assets/Scripts/Plante/GrowthManager.cs
+++ RootOfLife/Assets/Scripts/Plante/GrowthManager.cs
@@ -8,18 +8,13 @@ public class GrowthManager : MonoBehaviour
     public int maxCap = 12;
     public int currentCap;
     //GameObject lastChild;
-
+    Transform lastChild;
     GrowBehaviour test;
-
-    void Start()
-    {
-
-    }
 
     void Update()
     {
         currentCap = this.gameObject.transform.childCount;
-        Transform lastChild = this.gameObject.transform.GetChild(this.gameObject.transform.childCount - 1);
+        lastChild = this.gameObject.transform.GetChild(this.gameObject.transform.childCount - 1);
 
         if (currentCap >= maxCap)
         {
@@ -30,10 +25,34 @@ public class GrowthManager : MonoBehaviour
             //Lance une coroutine de destruction de child
         }
 
-        if(Input.GetKeyDown(KeyCode.H))
-            Destroy(lastChild.gameObject);
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            StartCoroutine("DestroyRoots");
+        } 
+
+        if (currentCap != maxCap)
+        {
+            if (!Input.GetKey(KeyCode.G))
+            {
+                Debug.Log("yolo");
+                //Ici on instancie le pont et on lance la coroutine de destruction des capsules? 
+            }
+        }
+
+        if (currentCap == 1)
+        {
+            StopCoroutine("DestroyRoots");
+        }
     }
 
     //Coroutine de destruction de child
     //Prend le dernier child (le + récent) et le détruit, puis au bout de 0.1 sec détruit le prochain child
+    IEnumerator DestroyRoots()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            Destroy(lastChild.gameObject);
+        }
+    }
 }
