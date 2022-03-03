5c784b09d01c89d656302bc55d91830b9171b03d
Hugo <kerdranvat.hugo@gmail.com>
2022-03-03 02:31:38 +0100

    Ajout destruction du pont overtime
    - temps = int secondes (modifiable à souhait)
    - la couleur du pont s'assombrit overtime jusqu'à destruction (visuel)
    - timer fait (coroutine)
    - modif prefab pont & mat grid + add script timerpont

---
+++ RootOfLife/Assets/Scripts/Plante/Pont/TimerPont.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerPont : MonoBehaviour
+{
+
+    Color currentColor;
+    MeshRenderer myRenderer;
+
+    public Color startColor, endColor;
+    public float colorChangeTime;
+    public int seconds;
+
+
+    void Awake()
+    {
+        StartCoroutine("Countdown");
+        myRenderer = GetComponent<MeshRenderer>();
+        myRenderer.material.color = startColor;
+        currentColor = startColor;
+        colorChangeTime = 0.0025f;
+    }
+
+    private void Update()
+    {
+        if (currentColor == startColor)
+        {
+            currentColor = endColor;
+        }
+        myRenderer.material.color = Color.Lerp(myRenderer.material.color, currentColor, colorChangeTime);
+    }
+
+    void DoStuff()
+    {
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator Countdown()
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+        DoStuff();
+    }
+}
