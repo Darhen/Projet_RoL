b10139bb190c0a5ce73adf1112b28a36cd3f7748
francoisl77 <98108051+francoisl77@users.noreply.github.com>
2022-03-04 15:54:32 -0500

    Mort fonctionnelle

--- RootOfLife/Assets/Scripts/respawn.cs
+++ RootOfLife/Assets/Scripts/respawn.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class respawn : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
-    void OntriggerEnter(Collider other)
+    public GameObject fadeToBlack;
+
+    void OnTriggerEnter(Collider other)
     {
-        player.transform.position = respawnPoint.transform.position;
+        if(other.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.transform.position;
+            Physics.SyncTransforms();
+            fadeToBlack.GetComponent<Animation>().Play("fadeToBlack");
+        }
     }
+    /*
+    public void FadingOut()
+    {
+        fadeToBlack.GetComponent<Animation>().Play("fadeToBlack");
+    }
+    */
 }
