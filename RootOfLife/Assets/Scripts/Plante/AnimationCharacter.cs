cfa47f9cb6095573f8ecf1c0689f637c0d922143
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-03-10 17:17:41 -0500

    script animation

--- RootOfLife/Assets/Scripts/Plante/AnimationCharacter.cs
+++ RootOfLife/Assets/Scripts/Plante/AnimationCharacter.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animation : MonoBehaviour
+public class AnimationCharacter : MonoBehaviour
 {
+    public GameObject avatar;
+    public Animator animator;
+
+    //BOOLS
+    public bool isLedgeClimbing;
+
+    //SCRIPTS
+    LedgeClimb ledgeClimb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        avatar = GameObject.Find("TestCharacter27_janvier");
+        ledgeClimb = GetComponent<LedgeClimb>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Animation ledge climb
+        isLedgeClimbing = ledgeClimb.isLedgeClimbing;
+        if (isLedgeClimbing)
+        {
+            animator.Play("LedgeClimb");
+        }
     }
 }
