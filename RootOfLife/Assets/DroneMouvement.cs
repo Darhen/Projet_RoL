30659b0a5372b65401eeab2e91f056ec9e5e6638
AlizeeBG <98108104+AlizeeBG@users.noreply.github.com>
2022-03-14 12:51:35 -0400

    Drone: allez-retour

--- RootOfLife/Assets/DroneMouvement.cs
+++ RootOfLife/Assets/DroneMouvement.cs
@@ -13,70 +13,76 @@ public class DroneMouvement : MonoBehaviour
     public Vector3 targetRotation;
 
 
-
     void Start()
     {
-        speed = 0.5f;
+        speed = 0.1f;
         isGoingBack = false;
         rotationSpeed = 0.25f;
         initialRotation = new Vector3(0, 0, 0);
         targetRotation = new Vector3 (0, 180, 0);
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.gameObject.transform.position.x > targetPosition.position.x && isGoingBack == false)
+        if (this.gameObject.transform.position.x > targetPosition.position.x && isGoingBack == false)
         {
             this.gameObject.transform.Translate(-speed, 0, 0);
+
+            if (this.gameObject.transform.position.x <= targetPosition.position.x)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 180, transform.rotation.z));
+                /*
+                speed = 0f;
+                this.gameObject.transform.Rotate(targetRotation * rotationSpeed * Time.deltaTime);
+
+                if (this.gameObject.transform.localEulerAngles.y >= 180f)
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 180, transform.rotation.z));
+                    rotationSpeed = 0f;
+                    speed = -0.25f;
+                }*/
+
+            }
         }
 
-        else if(this.gameObject.transform.position.x <= initialPosition.position.x)
+        else if (this.gameObject.transform.position.x <= initialPosition.position.x)
         {
-            this.gameObject.transform.Translate(speed, 0, 0);
+            this.gameObject.transform.Translate(-speed, 0, 0);
             isGoingBack = true;
 
+            if (this.gameObject.transform.position.x >= initialPosition.position.x)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0, transform.rotation.z));
+                /*
+                  speed = 0f;
+                  this.gameObject.transform.Rotate(targetRotation * rotationSpeed * Time.deltaTime);
+
+                  if (this.gameObject.transform.localEulerAngles.y <= 0f)
+                  {
+                      transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0, transform.rotation.z));
+                      rotationSpeed = 0f;
+                      Debug.Log("Allo");
+                      speed = -0.25f;
+                  }*/
+            }
         }
+
         else
         {
             isGoingBack = false;
         }
 
 
-        if (this.gameObject.transform.position.x >= initialPosition.position.x)
-        {
-            speed = 0f;
-            this.gameObject.transform.Rotate(targetRotation * -rotationSpeed * Time.deltaTime);
-            //this.gameObject.transform.rotation = Quaternion.Lerp(targetRotation, initialRotation, rotationSpeed * Time.deltaTime);
-            //this.gameObject.transform.Rotate(new Vector3(0, 0, 0), rotationSpeed * Time.deltaTime,Space.World);
-            //this.gameObject.transform.rotation = Quaternion.Lerp(this.gameObject.transform.rotation, Quaternion.Euler(new Vector3(0, 0, 0)), rotationSpeed * Time.deltaTime);
-
-            if (this.gameObject.transform.localEulerAngles.y <= 0f)
-            {
-                rotationSpeed = 0f;
-                speed = 0.5f;
-            }
-            
-        }
-
-        else if (this.gameObject.transform.position.x <= targetPosition.position.x)
-        {
-            speed = 0f;
-            this.gameObject.transform.Rotate(targetRotation * rotationSpeed * Time.deltaTime);
-            //this.gameObject.transform.rotation = Quaternion.Euler(targetRotation) * rotationSpeed * Time.deltaTime);
-            //this.gameObject.transform.Rotate(new Vector3(0, 180, 0), rotationSpeed * Time.deltaTime, Space.World);
-            //this.gameObject.transform.rotation = Quaternion.Lerp(this.gameObject.transform.rotation, Quaternion.Euler(new Vector3(0, 180, 0)), rotationSpeed * Time.deltaTime);
-
-            if (this.gameObject.transform.localEulerAngles.y >= 180f)
-            {
-                rotationSpeed = 0f;
-                speed = -0.5f;
-                
-            }  
-            
-        }
-
     }
+    /* private void OnTriggerEnter(Collider other)
+     {
+         if(other.gameObject.tag == "DronePos")
+         {
+             this.gameObject.transform.position = initialPosition.position;
+         }
+     }*/
 
 }
