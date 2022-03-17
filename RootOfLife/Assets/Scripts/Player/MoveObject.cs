5d6e7e859611014d3b6d5009dd0b3896a13b8b9a
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-03-17 12:14:04 -0400

    push robot drone et spider

--- RootOfLife/Assets/Scripts/Player/MoveObject.cs
+++ RootOfLife/Assets/Scripts/Player/MoveObject.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class MoveObject : MonoBehaviour
 {
     public bool playerIsPushing;
+    public bool canPush;
+    public bool pushingController;
+    public int direction;
+    public Vector3 edgeBox;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +19,17 @@ public class MoveObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //définir le input en x (direction a l'entrée dans le trigger)
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            direction = 1;
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            direction = -1;
+        }
 
-
+        //définir si le player utilise le input pour pousser
         if (Input.GetKey(KeyCode.P) || Input.GetButton("Fire3"))
         {
             playerIsPushing = true;
@@ -26,7 +39,38 @@ public class MoveObject : MonoBehaviour
             playerIsPushing = false;
         }
 
+        //définir si le player est entrain de pousser (controller)
+        if(canPush && playerIsPushing)
+        {
+            pushingController = true;
+        }
 
+    }
 
+    private void FixedUpdate()
+    {
+        //Controller dédié aux box
+        if (pushingController)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "box")
+        {
+            canPush = true;
+            edgeBox = other.gameObject.transform.position;
+        }
+        else
+        {
+            canPush = false;
+        }
     }
 }
