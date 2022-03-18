e4cec729ec6676e43d3e7619c88222e688ec1967
Jewhite2402 <jeremiewhite24@gmail.com>
2022-03-17 21:01:37 -0400

    add gym jé
    city add montagne, liane, skybox,

---
+++ RootOfLife/Assets/ControllerMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMenu : MonoBehaviour
+{
+    // Update is called once per frame
+    public void Clavier()
+    {
+        Debug.Log("Loading clavier image...");
+    }
+
+    public void Remote()
+    {
+        Debug.Log("Loading remote image...");
+    }
+}
