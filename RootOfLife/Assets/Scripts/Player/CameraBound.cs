58c07a3f8f26215d245c4bfd4bb06582e8cdb184
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-03-24 18:46:47 -0400

    LEVEL 1 SAFE -- DONT FUCK WITH IT

---
+++ RootOfLife/Assets/Scripts/Player/CameraBound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBound : MonoBehaviour
+{
+    public bool leftBoundary;
+    public bool rightBoundary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
