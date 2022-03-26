61afd2d950e6e9fb159fc3d8a6dd1a2c32c3ae17
EvanBurman-TheOrcaBird <92995324+EvanBurman-TheOrcaBird@users.noreply.github.com>
2022-03-26 10:36:20 -0400

    Soundbank shenanigans

--- RootOfLife/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ RootOfLife/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,10 +11,51 @@
 
 namespace AK
 {
+    namespace SWITCHES
+    {
+        namespace ENEMYALERTLEVEL
+        {
+            static const AkUniqueID GROUP = 2484586051U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALERT = 721787521U;
+                static const AkUniqueID NOTALERT = 3261712890U;
+            } // namespace SWITCH
+        } // namespace ENEMYALERTLEVEL
+
+        namespace SURFACES
+        {
+            static const AkUniqueID GROUP = 3714303081U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID SAND = 803837735U;
+            } // namespace SWITCH
+        } // namespace SURFACES
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MASTER_VOLUME = 4179668880U;
+        static const AkUniqueID MUSIC_VOLUME = 1006694123U;
+        static const AkUniqueID SFX_VOLUME = 1564184899U;
+    } // namespace GAME_PARAMETERS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID AMBIENT_REVERB = 3036939274U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
