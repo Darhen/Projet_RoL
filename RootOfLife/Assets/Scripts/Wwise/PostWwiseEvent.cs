a4bbba116efa9550d7b5aae58639db745de33d4d
Hugo <kerdranvat.hugo@gmail.com>
2022-03-27 01:35:51 +0100

    test wwise

---
+++ RootOfLife/Assets/Scripts/Wwise/PostWwiseEvent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWwiseEvent : MonoBehaviour
+{
+
+    public AK.Wwise.Event MyEvent;
+
+
+    public void PlayFootstepSound()
+    {
+        MyEvent.Post(gameObject);
+    }
+}
