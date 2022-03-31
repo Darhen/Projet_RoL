e1444cfec09767c2f62283893ffd779c309ee150
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-03-31 15:48:24 -0400

    update level 1 et duplicate du script timerPont

--- RootOfLife/Assets/Scripts/Interactable/LEVEL1/CameraTrigger1.cs
+++ RootOfLife/Assets/Scripts/Interactable/LEVEL1/CameraTrigger1.cs
@@ -7,11 +7,16 @@ public class CameraTrigger1 : MonoBehaviour
     public GameObject mainCamera;
     CameraFollow cameraFollow;
     public Vector3 cinematicOffset;
+    public bool activeCinematique;
+    public float cinematicDuration;
+    PlayerController playerController;
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraFollow = GameObject.FindWithTag("MainCamera").GetComponent<CameraFollow>();
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        cameraFollow = mainCamera.GetComponent<CameraFollow>();
+        playerController = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,9 +27,24 @@ public class CameraTrigger1 : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player" && activeCinematique)
+        {
+            StartCoroutine("cinematique");
+        }
+        if (other.gameObject.tag == "Player" && !activeCinematique)
         {
             cameraFollow.cinematicOffset = cinematicOffset;
         }
+    }
+
+    IEnumerator Cinematique()
+    {
+        cameraFollow.cinematicOffset = cinematicOffset;
+        playerController.enabled = false;
+        yield return new WaitForSeconds(cinematicDuration);
+        cinematicOffset = new Vector3(0, 0, 0);
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(1f);
+        playerController.enabled = true;
     }
 }
---
+++ RootOfLife/Assets/Scripts/Plante/Pont/TimerPontSafe.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerPontSafe : MonoBehaviour
+{
+
+    Color currentColor;
+    MeshRenderer myRenderer;
+
+    public Color startColor, endColor;
+    public float colorChangeTime;
+    public int seconds;
+
+    GrowthManager growthManager;
+    GameObject player;
+
+
+    void Awake()
+    {
+        StartCoroutine("Countdown");
+        myRenderer = GetComponent<MeshRenderer>();
+        myRenderer.material.color = startColor;
+        currentColor = startColor;
+        colorChangeTime = 0.0035f;
+
+        player = GameObject.FindWithTag("Player");
+        growthManager = player.GetComponentInChildren<GrowthManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentColor == startColor)
+        {
+            currentColor = endColor;
+        }
+        myRenderer.material.color = Color.Lerp(myRenderer.material.color, currentColor, colorChangeTime);
+    }
+
+    void DoStuff()
+    {
+        Destroy(this.gameObject);
+        //growthManager.StartCoroutine("DestroyRoots");
+    }
+
+    IEnumerator Countdown()
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+        DoStuff();
+    }
+}
