10346f17de377b99cd7c986fa4d5927de961bd1b
Hugo <kerdranvat.hugo@gmail.com>
2022-04-02 02:49:30 +0200

    Fix BrasRobot (déplacement et conflit avec le script de detection fix)

--- RootOfLife/Assets/Scripts/enemy/BrasMove.cs
+++ RootOfLife/Assets/Scripts/enemy/BrasMove.cs
@@ -14,11 +14,11 @@ public class BrasMove : MonoBehaviour
 
     public int movedTimes;
     Animator MyAnim;
+    public Animator AnimLight;
 
     private void Start()
     {
         MyAnim = GetComponentInChildren<Animator>();
-
         movedTimes = 0;
     }
     // Update is called once per frame
@@ -26,7 +26,8 @@ public class BrasMove : MonoBehaviour
     {
         if (isMoving)
         {
-            MyAnim.enabled = false;
+            //MyAnim.enabled = false;
+            //AnimLight.enabled = false;
             float step = speed * Time.deltaTime; // calculate distance to move
 
             if (movedTimes == 0)
@@ -35,7 +36,6 @@ public class BrasMove : MonoBehaviour
                 if (Vector3.Distance(transform.position, target1.position) < 0.001f)  // Check if the position of the cube and sphere are approximately equal.
                 {
                     transform.position = target1.transform.position;
-                    MyAnim.enabled = true;
                     isMoving = false;
                     movedTimes++;
                 }
@@ -44,24 +44,28 @@ public class BrasMove : MonoBehaviour
             if (movedTimes == 1)
             {
                 transform.position = Vector3.MoveTowards(transform.position, target2.position, step);
-                if (Vector3.Distance(transform.position, target2.position) < 0.001f)  
+                if (Vector3.Distance(transform.position, target2.position) < 0.001f)
                 {
                     transform.position = target2.transform.position;
                     movedTimes++;
                 }
             }
 
-            if(movedTimes == 2)
+            if (movedTimes == 2)
             {
                 transform.position = Vector3.MoveTowards(transform.position, target3.position, step);
-                if (Vector3.Distance(transform.position, target2.position) < 0.001f)  
+                if (Vector3.Distance(transform.position, target3.position) < 0.001f)
                 {
                     transform.position = target3.transform.position;
-                    MyAnim.enabled = true;
                     isMoving = false;
                     movedTimes++;
                 }
             }
+        }
+        else
+        {
+            //MyAnim.enabled = true;
+            //AnimLight.enabled = true;
         }
     }
 }
