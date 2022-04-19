587e3e69f657595576a3ed6f475abdada9a705df
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-04-19 11:27:51 -0400

    Modifs Finales Level 1 (READY POUR SON)

--- RootOfLife/Assets/Scripts/Interactable/LEVEL1/SalleFinale.cs
+++ RootOfLife/Assets/Scripts/Interactable/LEVEL1/SalleFinale.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SalleFinale : MonoBehaviour
 {
     public Animator salleFinaleAnimator;
+    private GameObject player;
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        playerRb = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,14 @@ public class SalleFinale : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             salleFinaleAnimator.enabled = true;
+            playerRb.isKinematic = true;
+            StartCoroutine("LoadingNextScene");
         }
+    }
+
+    IEnumerator LoadingNextScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("LEVEL2");
     }
 }
