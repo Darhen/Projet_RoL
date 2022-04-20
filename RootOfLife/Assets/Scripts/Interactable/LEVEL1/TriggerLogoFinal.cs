9942e3d0cfbd03dd0cc8bc62e0659b36ae0f4779
ManlyCresent02 <williamrobitaille@hotmail.com>
2022-04-20 18:01:05 -0400

    update level 1

--- RootOfLife/Assets/Scripts/Interactable/LEVEL1/TriggerLogoFinal.cs
+++ RootOfLife/Assets/Scripts/Interactable/LEVEL1/TriggerLogoFinal.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TriggerLogoFinal : MonoBehaviour
 {
     public GameObject sprite;
+    public GameObject sprite2;
+    public GameObject sprite3;
+    public GameObject sprite4;
     public Animator gateAnimator;
 
     // Start is called before the first frame update
@@ -24,6 +27,9 @@ public class TriggerLogoFinal : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             sprite.SetActive(true);
+            sprite2.SetActive(true);
+            sprite3.SetActive(true);
+            sprite4.SetActive(true);
             gateAnimator.SetTrigger("close");
             GetComponent<BoxCollider>().enabled = false;
         }
